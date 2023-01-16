e079fc03f297fc4bf738ccdf8be78206f4a2d400
Serdar Karacay <serdarkaracay@Serdars-Mac-Studio.local>
2023-01-16 15:22:20 +0000

    Configration Properties

---
+++ src/main/java/com/example/graalvmaot/GraalvmaotApplication.java
@@ -0,0 +1,13 @@
+package com.example.graalvmaot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmaotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmaotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graalvmaot/GraalvmaotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmaot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmaotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
